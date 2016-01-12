2e2cacb84857563663b07d67bd3552e9121a73e6
maxbert <mbertfield@stuy.edu>
2016-01-12 10:34:15 -0500

    created a test function for stock prices

---
+++ Testnum.java
@@ -0,0 +1,32 @@
+public class Testnum{
+    public static void  creatnum(double init, double volatility, double momentum){
+	double oldPrice = init;
+	double  newPrice =init;
+	for(int i = 0; i < 52; i++){
+	    oldPrice= newPrice;
+	    double rnd = Math.random();
+
+	    double changePercent = 2 * volatility * rnd;
+
+	    if (changePercent > volatility) {
+		changePercent -= (2 * volatility);
+	    }
+	    double  changeAmount = oldPrice * changePercent/100;
+	    newPrice = oldPrice + changeAmount;
+	    /*if (newPrice < MIN_PRICE) {
+		newPrice += Math.abs(changeAmount) * 2;
+	    } else if (newPrice > MAX_PRICE) {
+		newPrice -= Math.abs(changeAmount) * 2;
+	    }
+	    */
+	    System.out.println(newPrice);
+
+	}
+    }
+
+	
+    public static void main(String[] args){
+	creatnum(900,2.3,0.0);
+    }
+}
+	
