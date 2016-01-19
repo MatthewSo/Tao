cc4eba056127a441f87215046fea5365125c99ae
maxbert <mbertfield@stuy.edu>
2016-01-19 11:35:09 -0500

    added a driver

---
+++ Driver.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Driver{
+    private ArrayList<Double> list1 = new ArrayList<Double>();
+    list1.add(10);
+    list1.add(20);
+    list1.add(15);
+    list1.add(12);
+    private Stock stock1 = new Stock(Goog,20,1.2,1,list1);
+    stocks.add(stock1);
+    private int difficulty = 0;
+    private ArrayList<Stock> stocks = new ArrayList<Stock>();
+    
+    public void chooseDifficulty(int i){
+	difficulty = i;
+    }
+    public void view(ArrayList<Stock> st){
+	for(Stock a:st){
+	    System.out.println(st.name + "|" + st.curVal);
+	}
+    }
+	
+    public void buy(){
+	ArrayList<Double> ``
+	view(sto
