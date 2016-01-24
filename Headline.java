6ebc952260c41d39b24d66523979819bdd396677
Matthew So <mso@stuy.edu>
2016-01-24 11:05:09 -0500

    Added Headline.java

---
+++ Headline.java
@@ -0,0 +1,12 @@
+public class Headline{
+    Headline(String n, String v, double w, boolean cM){
+	noun = n;
+	verb = v;
+	worth = w;
+	changesMomentum = cM;
+    }
+    String noun;
+    String verb;
+    double worth;
+    boolean changesMomentum;
+}
