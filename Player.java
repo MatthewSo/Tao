1992843536df49305d4b740d8748b23a369d7080
Matthew So <mso@stuy.edu>
2016-01-24 12:55:01 -0500

    Added Player history functionality

--- Player.java
+++ Player.java
@@ -16,4 +16,19 @@ public class Player{
 	double temp1 = money;
 	money = m;
 	return temp1;}
+    ArrayList<Double> none = new ArrayList<Double>();
+    Stock history = new Stock("Personal Wealth",m,0.0,0.0,none, 0.0,"","");
+    public void newWorth(Double d){
+	history.addPrice( d);}
+    public String toString(){
+	String retstr = "Current value : " + curVal; //" %change : " + ((curVal - prices.get(prices.size()))/100) + "\n";
+	Graph g = new Graph(this);
+	retstr += g.printGraph();
+	return retstr;}
+
+    public static void main(String args[]){
+	Player me = new Player("Matthew So", "Hard", 100);
+	for (int i = 100; i < 200; i++){
+	    me.newWorth(i);}
+	System.out.println( me.toString());}
 }
